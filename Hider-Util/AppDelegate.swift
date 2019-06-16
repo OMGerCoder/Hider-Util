0bc857928293683f608f37eeefbb55c3d480f93a
Hugh <hugh.stuckless@hotmail.com>
2019-06-16 17:33:19 -0400

    Initial commit

---
+++ Hider-Util/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hider-Util
+//
+//  Created by Hugh Stuckless on 6/16/19.
+//  Copyright © 2019 Hugh Stuckless. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hider-Util/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hider-Util
+//
+//  Created by Hugh Stuckless on 6/16/19.
+//  Copyright © 2019 Hugh Stuckless. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
